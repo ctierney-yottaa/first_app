cfddc012a7eff2e41bde9b902cce9fcc1d77c891
Chris Tierney <c.tierney@yottaa.com>
2014-06-26 17:06:27 -0400

    Adding skeleton project and adding postgres config in database.yml and pg gem.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '1854c11907d2c0d0ffc3f855161eafbdf14168664095fd31620617dc9e03b0effd8d5cca13cc771a129ee13eaf2e0ad1e37c97c9cd03b5d5d558b6383af5c90f'
